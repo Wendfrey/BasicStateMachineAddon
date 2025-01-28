f8396a2efaf6ec83e31e276f9cace906cee7645c
Wendfrey <toni.cano.redondo@gmail.com>
2025-01-28 20:29:02 +0100

    Updated tab name and used constants for plugin.gd strings

--- addons/basic_fsm/basic_fsm_plugin.gd
+++ addons/basic_fsm/basic_fsm_plugin.gd
@@ -2,7 +2,8 @@
 extends EditorPlugin
 
 const RESOURCE_EDITOR = preload("res://addons/basic_fsm/ui/state_machine_resource_editor.tscn")
-
+const resourceEditorName = "Basic FSM Editor"
+const stateMachineNodeName = "StateMachineNode"
 var bottom_dock:Control
 
 func _enter_tree():
@@ -11,15 +12,17 @@ func _enter_tree():
 	if EditorInterface.get_inspector().get_edited_object() is StateMachineResource:
 		_on_resource_selected()
 		
-	add_custom_type("StateMachineNode", "Node", preload("res://addons/basic_fsm/StateMachineNode.gd"), preload("res://icon.svg"))
+	add_custom_type(stateMachineNodeName, "Node", preload("res://addons/basic_fsm/StateMachineNode.gd"), preload("res://icon.svg"))
 
 func _exit_tree():
 	EditorInterface.get_inspector().edited_object_changed.disconnect(_on_resource_selected)
-	remove_tool_menu_item("Test")
 	if bottom_dock:
 		if bottom_dock.is_inside_tree():
 			remove_control_from_bottom_panel(bottom_dock)
 		bottom_dock.free()
+		
+	remove_tool_menu_item(resourceEditorName)
+	remove_custom_type(stateMachineNodeName)
 
 func _on_resource_selected():
 	if EditorInterface.get_inspector().get_edited_object() is StateMachineResource:
@@ -27,8 +30,7 @@ func _on_resource_selected():
 			bottom_dock = RESOURCE_EDITOR.instantiate()
 		bottom_dock.request_ready()
 		if not bottom_dock.is_inside_tree():
-			add_control_to_bottom_panel(bottom_dock, "Test")
+			add_control_to_bottom_panel(bottom_dock, resourceEditorName)
 	elif bottom_dock:
 		remove_control_from_bottom_panel(bottom_dock)
 		
-	remove_custom_type("StateMachineNode")
